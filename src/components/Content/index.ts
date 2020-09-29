69347bae0453beb26ce81e11541303f6736ef00f
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-09-29 03:44:13 -0300

    responsiveness

--- src/components/Content/index.ts
+++ src/components/Content/index.ts
@@ -3,4 +3,12 @@ import styled from 'styled-components'
 export default styled.div`
   width: 920px;
   padding-top: 80px;
+
+  @media (max-width: 920px) {
+    width: 700px;
+  }
+
+  @media (max-width: 700px) {
+    width: 420px;
+  }
 `
