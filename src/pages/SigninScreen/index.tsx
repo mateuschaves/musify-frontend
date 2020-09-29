9cf27b89f8063ff8c24c238ec4c1096b74639e21
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-09-29 01:11:49 -0300

    validating signin form

--- src/pages/SigninScreen/index.tsx
+++ src/pages/SigninScreen/index.tsx
@@ -1,52 +1,61 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import { signInActions } from "../../store/ducks/Auth/signIn";
+import { signInActions } from '../../store/ducks/Auth/signIn'
 
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 
-import { useDispatch } from "react-redux";
+import { toast } from 'react-toastify'
 
-import { Container, Input, Title, Form, Button } from "./styles";
+import { useDispatch } from 'react-redux'
+
+import { Container, Input, Title, Form, Button } from './styles'
 
 export default function SigninScreen() {
-  const dispatch = useDispatch();
-  const history = useHistory();
+  const dispatch = useDispatch()
+  const history = useHistory()
 
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
 
   function handleSignIn(email: string, password: string) {
+    if (!validateForm(email, password))
+      return toast('Informe seu login e senha corretamente', { type: 'error' })
     dispatch(
       signInActions.signIn({
         email,
         password,
-        history,
+        history
       })
-    );
+    )
+  }
+
+  function validateForm(email: string, password: string) {
+    if (!email || !password) return false
+    else return true
   }
 
   return (
     <Container>
       <Form>
-        <Title>{["Entre agora mesmo", "no Musify"].join("\n")}</Title>
+        <Title>{['Entre agora mesmo', 'no Musify'].join('\n')}</Title>
 
         <Input
-          placeholder="Email"
+          placeholder='Email'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          type="email"
+          type='email'
         />
         <Input
-          placeholder="Senha"
+          placeholder='Senha'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          type="password"
+          type='password'
         />
 
-        <Button type="button" onClick={() => handleSignIn(email, password)}>
+        <Button type='button' onClick={() => handleSignIn(email, password)}>
           ENTRAR
         </Button>
       </Form>
     </Container>
-  );
+  )
 }
