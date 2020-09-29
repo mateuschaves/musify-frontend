c1073c22ad28c96ce194d7df537630fea8f4efa9
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-09-29 04:16:00 -0300

    server in .js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import favicon from 'express-favicon'
-import path from 'path'
+const express = require('express')
+const favicon = require('express-favicon')
+const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 app.use(favicon(__dirname + '/build/favicon.ico'))
